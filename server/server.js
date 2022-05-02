797a7fe120cb91abcba51ba5d13357e4fdbab724
cadmium <constantine.dm@gmail.com>
2022-05-02 15:41:23 -0400

    fix config options

--- server/server.js
+++ server/server.js
@@ -11,9 +11,9 @@ const config = {
     authRequired: false,
     auth0Logout: true,
     secret: process.env.AUTH0_SECRET,
-    // baseURL: process.env.BASE_URL,
+    baseURL: process.env.BASE_URL,
     clientID: process.env.AUTH0_CLIENT_ID,
-    // issuerBaseURL: process.env.ISSUER_BASE_URL
+    issuerBaseURL: process.env.ISSUER_BASE_URL
   };
 
 app.use(auth(config));
