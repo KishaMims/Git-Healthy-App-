e9b879d1b52c6d4887ad044c3e746410294415af
KishaMims <mimsn.denise@gmail.com>
2022-05-05 13:49:32 -0500

    mealfile

---
+++ client/src/pages/meals.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function MealTime(props) {
+  return (
+        <div>
+            <h1>Your Daily Nurition Summary</h1>
+        </div>
+      );
+    }
+    
