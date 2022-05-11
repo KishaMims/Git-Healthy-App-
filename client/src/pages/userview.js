144fc4513491a8d47885405c68930653ba37b6b6
KishaMims <mimsn.denise@gmail.com>
2022-05-10 21:36:48 -0500

    date

--- client/src/pages/userview.js
+++ client/src/pages/userview.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { useState, useEffect } from "react";
+import moment from 'moment';
 
 
 const UserView = ()=> {
 const [usermeals, setUserMeals] = useState([]);
 // const [users, setUser] = useState(undefined);
   const current = new Date();
-  const date = `${current.getDate()}/${current.getMonth()+1}/${current.getFullYear()}`;
+  const date = `${current.getMonth()+1}/${current.getDate()}/${current.getFullYear()}`;
 
   useEffect(() => {
     fetch('/api/meals')
@@ -36,7 +37,12 @@ const [usermeals, setUserMeals] = useState([]);
     <div className="App">
       <h1>Your Daily Nutrition Summary</h1>
       <h1>{date}</h1>
-      <>{JSON.stringify(usermeals)}</>
+      <ul>
+      {usermeals.map((meal, index) =>
+      <li className='list' key={index}> <br /> 
+        <h2>{meal.foodeaten}</h2><br /> {meal.calories}<br /> {meal.course} <br /> <br />Added on: {moment(meal.addedon).format('MM/DD/YYYY')} <br />
+      </li>)}
+      </ul>
     </div>
   );
 }
