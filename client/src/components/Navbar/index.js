26fde4b51785fa8057e7276366c0d73b919efba4
KishaMims <mimsn.denise@gmail.com>
2022-05-15 16:56:55 -0500

    hide userview link

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,38 +1,83 @@
 import React from "react";
 import { Nav, NavLink, NavMenu } from "./NavBarElements.js";
 import image from './logo.jpg';
+import { useState, useEffect } from "react";
+import Login from "../../pages/login.js";
 
 
+const Navbar = ( props ) => {
 
+const [hidelink, setHideLink] = useState(undefined);
 
+const login = () => {
+  setHideLink(false);
+}
 
+const [user, setUser] = useState(undefined);
 
-const Navbar = () => {
+const loadUser = () => {
+  fetch("/api/login")
+  .then((response) => {
+  if(response.status === 200){
+      return response.json()
+     
+  } else {
+    return undefined;
+  }
+  })
+  .then(user =>{
+      setUser(user);
+  })
+};
+
+useEffect(()=>{
+  loadUser();
+}, []);    
+
     return (
     <> 
     <Nav>
+    <ul>
         <NavMenu>
         <img src={image} alt="logo" height={80} style={{ align: !'left' }} />
         <h2 className="title-tag">Git Healthy</h2>
+        <li>
           <NavLink to="/about" activeStyle>
             About
           </NavLink>
+          </li>
+          <li>
           <NavLink to="/home" activeStyle>
             Home
           </NavLink>
-             
+          </li>
+             { user && 
+             <li className="hiddenLink">
                <NavLink to="/api/userview" activeStyle>
                 Meals
                 </NavLink>
-               
+                </li>
+                }
+                <li>
                 <NavLink to="/exercise" activeStyle>
                   Exercises
-                </NavLink><NavLink to="/api/recipes" activeStyle>
+                </NavLink>
+                </li>
+                <li>
+                <NavLink to="/api/recipes" activeStyle>
                   Recipes
-                </NavLink><NavLink to="/contact" activeStyle>
+                </NavLink>
+                </li>
+                <li>
+                <NavLink to="/contact" activeStyle>
                   Contact
                 </NavLink>
+                </li>
+                <li>
+                  <Login/>
+                </li>
         </NavMenu>
+        </ul>
     </Nav>
     </>
 )
--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -6,27 +6,7 @@ import { useState, useEffect } from 'react';
 
 export default function Login() {
 
-    const [user, setUser] = useState(undefined);
-    // const navigate = useNavigate();
-    // const { email } = useParams();
-
-
-    // const [allusers, setAllUsers] = useState([]);
-
-    
-    //     useEffect(() => {
-    //         fetch('http://localhost:3000/api/all/nutrition')
-    //            .then((response) => response.json())
-    //            //.then(response => response.text())
-    //             .then(allusers => {
-    //                 setAllUsers(allusers);
-    //                 console.log(allusers);
-    //             }
-    
-    //             )
-    
-    //     }, []);
-
+const [user, setUser] = useState(undefined);
    
     const loadUser = () => {
         fetch("/api/login")
@@ -47,18 +27,22 @@ export default function Login() {
         loadUser();
     }, []);    
 
-//     const handleOnClick = () => {
-//        fetch(`http://localhost:5000/api/nutrition/${email}`,{
-//        method: 'POST'
-//        }).then(()=> {
-//        navigate('/api/nutrition//meals')
-//       })
-//   };
 
   return (
     <div>
     <div>
-            {!user ? (<ul><button className="login"><a href="http://localhost:3001/login">Login</a></button></ul>) : (<ul>Welcome, {user.email} <button><a href="http://localhost:3001/logout">Logout</a></button></ul>)}
+
+            {!user ? (
+            <ul><button className="login">
+                <a href="http://localhost:3001/login">Login</a>
+                </button>
+                </ul>
+                ) : (
+                <ul>Welcome, {user.given_name}
+        <button><a href="http://localhost:3001/logout">Logout</a>
+        </button>
+        </ul>
+        )}
     </div>
     <div>
         {!user}
